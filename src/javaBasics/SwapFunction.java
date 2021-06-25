5341b211e541f46cbc3ef277c0500f91ea442bae
dorde018 <jorgetrajkovic@gmail.com>
2021-06-25 11:54:40 +0200

    Java basics

--- src/javaBasics/SwapFunction.java
+++ src/javaBasics/SwapFunction.java
@@ -9,10 +9,10 @@ public class SwapFunction{
 		String printStrings = swap("First String", "Second String", swap);
 	
 			if (swap) {
-				System.out.println("Strings are reversed: "  +  printStrings + ".");}
+				System.out.println("The strings are reversed: "  +  printStrings + ".");}
 			
 			else {
-				System.out.println("Strings are not reversed: "  +  printStrings + ".");}
+				System.out.println("The strings are not reversed: "  +  printStrings + ".");}
 	}
 	
 	public static String swap(String firstStr, String secondStr, boolean swap) {   
