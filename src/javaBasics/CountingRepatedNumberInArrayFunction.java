01697d09b685db4bbd2d93e2e568c9d979c40410
dorde018 <jorgetrajkovic@gmail.com>
2021-06-28 23:25:03 +0200

    Java basics

--- src/javaBasics/CountingRepatedNumberInArrayFunction.java
+++ src/javaBasics/CountingRepatedNumberInArrayFunction.java
@@ -8,19 +8,19 @@ import java.util.Scanner;
 			int [] arrayOfNumbers = { 6, 5, 12, 6, 8, 13, 4, 2, 18, 2, 44, 59, 6, 187, 11, 67, 8, 72 };
 			System.out.println("Enter the number you want to count in the array: ");
 			int selectedNumber = s.nextInt();
-			int numOfRepetitons = counterOfRepatedNumberInArray(arrayOfNumbers, selectedNumber);
+			int numOfRepetitions = counterOfRepeatedNumberInArray(arrayOfNumbers, selectedNumber);
 			
-			if (numOfRepetitons == 0) {
+			if (numOfRepetitions == 0) {
 				System.out.println("Number " + selectedNumber + " does not exist in the array.");
-			}else if(numOfRepetitons == 1) {
-				System.out.println("Number " + selectedNumber + " repeats " + numOfRepetitons + " time in the array.");
+			}else if(numOfRepetitions == 1) {
+				System.out.println("Number " + selectedNumber + " repeats " + numOfRepetitions + " time in the array.");
 			}
 			else {
-				System.out.println("Number " + selectedNumber + " repeats " + numOfRepetitons + " times in the array.");
+				System.out.println("Number " + selectedNumber + " repeats " + numOfRepetitions + " times in the array.");
 			}
 			System.out.println("\nThank you for using our program!");
 		}
-		public static int counterOfRepatedNumberInArray(int[] array, int selectedNumber) {
+		public static int counterOfRepeatedNumberInArray(int[] array, int selectedNumber) {
 			int counter = 0;
 			for (int i = 0; i < array.length; i++) {
 				if (array[i] == selectedNumber) {
